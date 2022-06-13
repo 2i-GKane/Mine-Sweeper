1b7b7195685406e300f2a375efa912bc81b884bb
2i-GKane <gabriel.kane@2itesting.com>
2022-06-13 19:49:04 +0100

    Rebase project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import { getGameBoard, getTileElements } from './Board';
+
+import "./styling.css";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRectangleXmark } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const boardSize = 10;
+
+  const mineCount = 10;
+
+  const[board, setBoard] = useState(getGameBoard(boardSize, mineCount));
+  const[score, setScore] = useState(0);
+
+  const tileElements = getTileElements();
+  document.querySelector(':root').style.setProperty('--board-size', boardSize);
+
+  const handleTileClick = (posX, posY) => {
+    const id = posX + "-" + posY;
+
+    const tileElement = document.getElementById(id);
+    tileElement.classList.remove("tile");
+    tileElement.classList.add("tile--clicked");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="window-asthetics">
+          <div className="window-info">
+            <h2>Minesweeper</h2>
+            <FontAwesomeIcon className="end-game" icon={faRectangleXmark}/>
+          </div>
+          <div className="game-container">
+            <div className="score-container">
+              <h1 className="score" id="score">{score}</h1>
+            </div>
+            <br className="score-container--padding-btm"/>
+
+            <div id="board" className="board-container">
+              {tileElements.map(element => {
+                const posX = element.posX;
+                const posY = element.posY;
+
+                return <div onClick={() => handleTileClick(posX, posY)} className="tile" id={posX + "-" + posY} board-size={boardSize} pos-x={posX} pos-y={posY}/>
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Board.js
@@ -0,0 +1,85 @@
+const gameBoard = [];
+let tileElements = [];
+
+const getTileElements = () => {
+    return tileElements;
+}
+
+const getMines = (boardSize, mines) => {
+    let minePositions = [];
+
+    while(minePositions.length < mines){
+        const randX = Math.floor(Math.random() * boardSize);
+        const randY = Math.floor(Math.random() * boardSize);
+
+        const mine = {
+            x: randX,
+            y: randY
+        }
+
+        if(!minePositions.includes({ mine })){
+            minePositions.push(mine);
+        }
+    }
+
+    return minePositions;
+}
+
+function checkIsMine(tile, mines){
+    let isMine = false;
+
+    mines.map(minePos => {
+        const minePosX = minePos.x;
+        const minePosY = minePos.y;
+
+        if(tile.x === minePosX && tile.y === minePosY) isMine = true;
+    })
+
+    return isMine;
+}
+
+const createTileElement = (tileX, tileY) => {
+    const tileElement = {
+        posX: tileX,
+        posY: tileY
+    }
+    return tileElement;
+}
+
+const getGameBoard = (size, mines) => {
+    if(mines > size) mines = size;
+    const currentMinePositions = getMines(size, mines);
+
+    tileElements = [];
+
+    for(let posX = 0; posX < size; posX++){
+        let currentRow = [];
+        for(let posY = 0; posY < size; posY++){
+
+            const isMine = checkIsMine({x: posX, y: posY}, currentMinePositions);
+            const tileElement = createTileElement(posX, posY);
+
+            tileElements.push(tileElement);
+
+            const gameTile = {
+                x: posX,
+                y: posY,
+                isMine: isMine,
+                isRevealed: false,
+                isFlagged: false
+            }
+
+            currentRow.push(gameTile);
+        }
+
+        gameBoard.push(currentRow);
+        
+    }
+
+
+    return gameBoard;
+}
+
+
+
+export { getGameBoard, getTileElements };
