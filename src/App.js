41b88d3c662fd55a3bc8ad20aa43acf3f8dff9b0
2i-GKane <gabriel.kane@2itesting.com>
2022-06-13 21:44:10 +0100

    Add flagging system

--- src/App.js
+++ src/App.js
@@ -5,25 +5,56 @@ import { getGameBoard, getTileElements } from './Board';
 import "./styling.css";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRectangleXmark } from "@fortawesome/free-solid-svg-icons";
+import { faRectangleXmark, faFlag } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+});
+
+const App = () => {
   const boardSize = 10;
-
   const mineCount = 10;
 
   const[board, setBoard] = useState(getGameBoard(boardSize, mineCount));
   const[score, setScore] = useState(0);
+  const[flags, setFlags] = useState(10);
+  const[remainingMines, setRemainingMines] = useState(mineCount);
 
   const tileElements = getTileElements();
   document.querySelector(':root').style.setProperty('--board-size', boardSize);
 
   const handleTileClick = (posX, posY) => {
+    const tile = board[posX][posY];
     const id = posX + "-" + posY;
 
     const tileElement = document.getElementById(id);
     tileElement.classList.remove("tile");
     tileElement.classList.add("tile--clicked");
+
+    if(tile.isFlagged) flagTile(posX, posY);
+  }
+
+  const flagTile = (posX, posY) => {
+    const tile = board[posX][posY];
+    const id = "flag-" + posX + "-" + posY;
+
+      if(!tile.isFlagged){
+        if(flags <= 0) return
+
+        tile.isFlagged = true;
+        setFlags((currentFlags) => currentFlags -1)
+        document.getElementById(id).style.display = "block";
+      } else {
+        tile.isFlagged = false;
+        setFlags((currentFlags) => currentFlags + 1)
+
+        document.getElementById(id).style.display = "none";
+      }
+
   }
 
   return (
@@ -35,8 +66,15 @@ function App() {
             <FontAwesomeIcon className="end-game" icon={faRectangleXmark}/>
           </div>
           <div className="game-container">
-            <div className="score-container">
-              <h1 className="score" id="score">{score}</h1>
+            <div className="stats-bar">
+              <div className="score-container">
+                <h1>Score: </h1>
+                <h1 className="score" id="score">{score}</h1>
+              </div>
+              <div className="flag-container">
+                <h1>Flags: </h1>
+                <h1 className="flags" id="flags">{flags}</h1>
+              </div>
             </div>
             <br className="score-container--padding-btm"/>
 
@@ -45,7 +83,7 @@ function App() {
                 const posX = element.posX;
                 const posY = element.posY;
 
-                return <div onClick={() => handleTileClick(posX, posY)} className="tile" id={posX + "-" + posY} board-size={boardSize} pos-x={posX} pos-y={posY}/>
+                return <div key={posX + "-" + posY} onClick={() => handleTileClick(posX, posY)} onContextMenu={() => flagTile(posX, posY)} className="tile" id={posX + "-" + posY} board-size={boardSize} pos-x={posX} pos-y={posY}><FontAwesomeIcon className="flag" id={"flag-" + posX + "-" + posY} icon={faFlag}/></div>
               })}
             </div>
           </div>
